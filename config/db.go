27ff850753b8c3cd3c9dd4f55abc1e2792076908
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-28 22:56:58 +0700

    init golang fiber mongodb

---
+++ config/db.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	gotenv "github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"os"
+	"time"
+)
+
+type MongoInstance struct {
+	Client *mongo.Client
+	DB     *mongo.Database
+}
+
+var MI MongoInstance
+
+func ConnectDB() {
+	if os.Getenv("APP_ENV") != "production" {
+		err := gotenv.Load()
+		if err != nil {
+			log.Fatal("error load file .env")
+		}
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("database connected")
+	MI = MongoInstance{
+		Client: client,
+		DB:     client.Database(os.Getenv("DB")),
+	}
+}
