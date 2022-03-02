7e8161eea49a8b57d4755f961b2adbeeef9b33be
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-03-02 19:00:20 +0700

    update controller, models, and main

--- controllers/catchphraseController.go
+++ controllers/catchphraseController.go
@@ -8,6 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"math"
 	"strconv"
 	"time"
@@ -105,5 +106,102 @@ func GetCatchphrase(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"data":    catchphrase,
 		"success": true,
+	})
+}
+
+func AddCatchphrase(c *fiber.Ctx) error {
+	catchphraseCollection := config.MI.DB.Collection("catchphrases")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	catchphrase := new(models.Catchphrase)
+	if err := c.BodyParser(catchphrase); err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(fiber.Map{
+			"status":  false,
+			"message": "Invalid request body",
+			"Error":   err,
+		})
+	}
+	result, err := catchphraseCollection.InsertOne(ctx, catchphrase)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  false,
+			"message": "Catchphrase failed to insert",
+			"Error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"data":    result,
+		"success": true,
+		"message": "catchphrase inserted successfully",
+	})
+}
+
+func UpdateCatchphrase(c *fiber.Ctx) error {
+	catchphraseCollection := config.MI.DB.Collection("catchphrases")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	catchphrase := new(models.Catchphrase)
+
+	if err := c.BodyParser(catchphrase); err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(fiber.Map{
+			"status":  false,
+			"message": "Invalid request body",
+			"Error":   err,
+		})
+	}
+
+	objId, err := primitive.ObjectIDFromHex(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  false,
+			"message": "Catchphrase not found",
+			"Error":   err,
+		})
+	}
+
+	update := bson.M{
+		"$set": catchphrase,
+	}
+	_, err = catchphraseCollection.UpdateOne(ctx, bson.M{"_id": objId}, update)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  false,
+			"message": "Catchphrase failed to update",
+			"Error":   err,
+		})
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"data":    catchphrase,
+		"success": true,
+		"message": "catchphrase updated successfully",
+	})
+}
+
+func DeleteCatchphrase(c *fiber.Ctx) error {
+	catchphraseCollection := config.MI.DB.Collection("catchphrases")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	objId, err := primitive.ObjectIDFromHex(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  false,
+			"message": "Catchphrase not found",
+			"Error":   err,
+		})
+	}
+
+	_, err = catchphraseCollection.DeleteOne(ctx, bson.M{"_id": objId})
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"status":  false,
+			"message": "Catchphrase failed to delete",
+			"Error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"success": true,
+		"message": "catchphrase deleted successfully",
 	})
 }
--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	gotenv "github.com/joho/godotenv"
+	"github.com/xvbnm48/mongodb-fiber/config"
+	"github.com/xvbnm48/mongodb-fiber/routes"
+	"log"
+	"os"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":  true,
+			"message": "you are at the root end point",
+		})
+	})
+	api := app.Group("/api")
+	routes.CatchprasesRoute(api.Group("/catchprases"))
+	app.Listen(":3000")
+}
 
 func main() {
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world")
-	})
+	if os.Getenv("APP_ENV") == "production" {
+		err := gotenv.Load()
+		if err != nil {
+			log.Fatalln("error loading env file")
+		}
+	}
 
-	app.Listen(":3000")
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(logger.New())
+
+	config.ConnectDB()
+
+	setupRoutes(app)
+
+	port := os.Getenv("PORT")
+	err := app.Listen(":" + port)
+	if err != nil {
+		log.Fatalln("error starting server")
+		panic(err)
+	}
 }
